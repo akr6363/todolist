f3224ff0e9ea86a61181468e972c15f2ab07a53d
Anna Krasilnikova <akr6363@mail.ru>
2023-07-18 13:26:15 +0300

    -fix bags

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -87,12 +87,12 @@ const App: React.FC = () => {
                         </Toolbar>
 
                     </AppBar>
-                    <Container fixed>
+                    <Container fixed sx={{mb:'20px'}}>
                         <Routes>
                             <Route path={'/'} element={<TodoListPage/>}/>
                             <Route path={'/login'} element={<Login/>}/>
-                            {/*<Route path={'/404'} element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>*/}
-                            {/*<Route path={'*'} element={<Navigate to={'/404'}/>}/>*/}
+                            <Route path={'/404'} element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>}/>
+                            <Route path={'*'} element={<Navigate to={'/404'}/>}/>
                         </Routes>
                     </Container>
                 </div>
